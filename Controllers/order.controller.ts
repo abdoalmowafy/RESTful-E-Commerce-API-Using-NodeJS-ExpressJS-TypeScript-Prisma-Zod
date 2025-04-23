5859ea749de895f6b34e27aade6dbc6d508966df
Abdelrahman Almowafy <abdoalmowafy@gmail.com>
2025-04-23 22:40:38 +0200

    feat(api): paymob service

---
+++ Controllers/order.controller.ts
@@ -0,0 +1,108 @@
+import verifyToken from "../Middlewares/verifyToken";
+import { globalClient as prisma } from "../prismaClient";
+import { request, Request, Response, Router } from "express";
+import { getValidatedCart } from "./cart.controller";
+import { z } from "zod";
+import { ExtendedOrderSchema } from "../Models/order.model";
+import validateZodSchema from "../Middlewares/validateZodSchema";
+import pay from "../Services/paymob.service";
+
+const orderController = Router();
+const orderRequestSchema = ExtendedOrderSchema.pick({
+    deliveryNeeded: true,
+    deliveryAddressId: true,
+    paymentMethod: true,
+    currency: true,
+}).extend({ identifier: z.string().length(11).optional() });
+type OrderRequest = z.infer<typeof orderRequestSchema>;
+
+const indexOrders = async (req: Request, res: Response) => {
+    const userId = req.params.userId;
+    const orders = await prisma.order.findMany({ where: { userId: userId }, include: { orderItems: true, deliveryAddress: true } });
+    res.status(200).json(orders);
+};
+orderController.get("/", verifyToken(), indexOrders);
+
+const validateAddress = async function (addressId: string, userId: string, deliveryNeeded: boolean) {
+    const deliveryAddress = deliveryNeeded ?
+        await prisma.address.findUnique({ where: { id: addressId, storeAddress: true, deleted: false } }) :
+        await prisma.address.findUnique({ where: { id: addressId, userId: userId, deleted: false } });
+
+    return deliveryAddress;
+}
+const isValidCurrency = function (currency: string): boolean {
+    return currency === "EGP";
+}
+
+
+const COD_FEE = 1000;
+const DELIVERY_FEE = 5000;
+const createOrder = async (req: Request, res: Response) => {
+    const userId = req.params.userId;
+    const userName = req.params.userName;
+    const userPhone = req.params.userPhone;
+    const userEmail = req.params.userEmail;
+    const cart = await getValidatedCart(userId);
+    const request: OrderRequest = req.body;
+    const deliveryAddress = await validateAddress(request.deliveryAddressId, userId, request.deliveryNeeded);
+
+    if (!deliveryAddress) {
+        res.status(400).json({ error: 'Invalid address' });
+        return;
+    }
+
+    if (isValidCurrency(request.currency)) {
+        res.status(400).json({ error: 'Invalid currency' });
+        return;
+    }
+
+    const orderItems = cart!.cartItems.map(ci => ({
+        productId: ci.productId,
+        productPriceCents: ci.product.priceCents,
+        productSalePercent: ci.product.salePercent,
+        warrantyDays: ci.product.warrantyDays,
+        quantity: ci.quantity,
+    }))
+
+    const totalCentsBeforePromo = orderItems.reduce((total, oi) =>
+        total + (oi.productPriceCents * oi.quantity * (1 - (oi.productSalePercent / 100))), 0)
+
+    let totalCents = cart!.promoCode ? totalCentsBeforePromo * (1 - (cart!.promoCode.discountPercent / 100)) : totalCentsBeforePromo;
+
+    totalCents += request.deliveryNeeded ? DELIVERY_FEE : 0;
+    totalCents += request.paymentMethod === "COD" ? COD_FEE : 0;
+
+    try {
+        const orderPromise = prisma.order.create({
+            data: {
+                userId: userId,
+                deliveryAddressId: request.deliveryAddressId,
+                promoCodeId: cart!.promoCodeId,
+                totalCents: totalCents,
+                status: request.paymentMethod === "COD" ? "PROCESSING" : "PAYING",
+                orderItems: {
+                    createMany: { data: orderItems }
+                },
+                deliveryNeeded: request.deliveryNeeded,
+                paymentMethod: request.paymentMethod,
+                currency: request.currency
+            }
+        });
+        const updateProductsPromises = cart!.cartItems.map(ci => prisma.product.update({
+            where: { id: ci.productId }, data: { stock: ci.product.stock - ci.quantity }
+        }));
+        const results = await prisma.$transaction([orderPromise, ...updateProductsPromises]);
+        const order = results[0];
+        if (order.paymentMethod !== "COD") {
+            res.status(200).json({ redirectURL: await pay(userName, userEmail, userPhone, order, orderItems, deliveryAddress, request.identifier) });
+            return;
+        }
+        res.status(200).json(order);
+    } catch (error) {
+        res.status(400).json({ error: "Error creating order" });
+    }
+};
+orderController.post("/", verifyToken(), validateZodSchema(orderRequestSchema), createOrder);
+
+
+
---
+++ Services/paymob.service.ts
@@ -0,0 +1,120 @@
+import { ExtendedAddress } from '../Models/address.model';
+import { OrderItem } from '../Models/generated/zod';
+import { ExtendedOrder } from '../Models/order.model';
+
+const baseUrl = 'https://accept.paymob.com/api/';
+const apiKey = process.env.PAYMOB_API_KEY!;
+const integrationId = parseInt(process.env.PAYMOB_INTEGRATION_ID!);
+const iframe1Id = process.env.PAYMOB_IFRAME1_ID!;
+
+async function post<T = any>(url: string, data: unknown): Promise<T> {
+    const response = await fetch(baseUrl + url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Request failed: ${response.statusText}`);
+    }
+
+    return response.json() as Promise<T>;
+}
+
+async function authenticate(): Promise<string> {
+    const data = await post<{ token: string }>('auth/tokens', {
+        api_key: apiKey,
+    });
+    return data.token;
+}
+
+async function registerOrder(order: ExtendedOrder, orderItems: Omit<OrderItem, "orderId">[], authToken: string): Promise<number> {
+    if (!order) throw new Error('Order is missing!');
+
+    const items = orderItems.map((op) => ({
+        amount_cents: op.productPriceCents * (1 - op.productSalePercent / 100),
+        quantity: op.quantity,
+    }));
+
+    const orderRequest = {
+        auth_token: authToken,
+        delivery_needed: order.deliveryNeeded,
+        amount_cents: order.totalCents,
+        currency: order.currency,
+        items,
+    };
+
+    const response = await post<{ id: number }>('ecommerce/orders', orderRequest);
+    return response.id;
+}
+
+async function requestPaymentKey(userName: string, userEmail: string, userPhone: string, order: ExtendedOrder, orderItems: Omit<OrderItem, "orderId">[], address: ExtendedAddress, orderId: number, authToken: string): Promise<string> {
+    if (!order) throw new Error('Order is missing!');
+
+    const billingData = {
+        apartment: address.apartment,
+        email: userEmail,
+        floor: address.floor,
+        first_name: userName.split(' ')[0],
+        street: address.street,
+        building: address.building,
+        phone_number: userPhone,
+        shipping_method: 'PKG',
+        postal_code: address.postalCode,
+        city: address.city,
+        country: address.country,
+        last_name: userName.split(' ').slice(-1)[0],
+        state: address.state,
+    };
+
+    const paymentKeyRequest = {
+        auth_token: authToken,
+        amount_cents: order.totalCents,
+        expiration: 3600,
+        order_id: orderId,
+        billing_data: billingData,
+        currency: order.currency,
+        integration_id: integrationId,
+    };
+
+    const response = await post<{ token: string }>('acceptance/payment_keys', paymentKeyRequest);
+    return response.token;
+}
+
+async function payMobileWallet(paymentKey: string, identifier: string): Promise<string> {
+    const request = {
+        source: {
+            identifier,
+            subtype: 'WALLET',
+        },
+        payment_token: paymentKey,
+    };
+
+    const response = await post<{ redirection_url: string }>('acceptance/payments/pay', request);
+    return response.redirection_url;
+}
+
+const pay = async (userName: string, userEmail: string, userPhone: string, order: ExtendedOrder, orderItems: Omit<OrderItem, "orderId">[], deliveryAddress: ExtendedAddress, identifier?: string): Promise<string> => {
+    if (!order) throw new Error('Order is missing!');
+    if (order.paymentMethod === "MOBILEWALLET" && !identifier) throw new Error('Identifier is missing!');
+
+    const authToken = await authenticate();
+    const orderId = await registerOrder(order, orderItems, authToken);
+    const paymentKey = await requestPaymentKey(userName, userEmail, userPhone, order, orderItems, deliveryAddress, orderId, authToken);
+
+    switch (order.paymentMethod) {
+        case 'COD':
+            throw new Error('Invalid method!');
+        case 'CREDITCARD':
+            return `https://accept.paymobsolutions.com/api/acceptance/iframes/${iframe1Id}?payment_token=${paymentKey}`;
+        case 'MOBILEWALLET':
+            if (!identifier) throw new Error('Identifier is missing!');
+            return await payMobileWallet(paymentKey, identifier);
+        default:
+            throw new Error('Not implemented');
+    }
+}
+
+export default pay;
